b9a5ba3306897e802f3e290ce7a5cc93f062de0d
kangting <424167481@qq.com>
2019-07-11 11:09:49 +0800

    update

--- miao-03.精品实战案例，四步搞定 VUE/courseware/5-8   2018-01-04公开课课件/project-vue/src/main.js
+++ miao-03.精品实战案例，四步搞定 VUE/courseware/5-8   2018-01-04公开课课件/project-vue/src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import store from './store'
+import store from './store/index'
 
 // 使用组件三部曲：引入组件、注册组件、使用组件
 
